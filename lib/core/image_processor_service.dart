cbbe75b66d920b49675daf375c7d3bbfb886b968
Min Pyae Phyo <subtominthebro@gmail.com>
2026-07-22 17:01:38 +0800

    Initial commit

---
+++ lib/core/image_processor_service.dart
@@ -0,0 +1,9 @@
+import 'package:opencv_dart/opencv_dart.dart' as cv;
+import 'dart:typed_data';
+
+class ImageProcessor{
+  Uint8List processImage(Uint8List rawImageBytes){
+    // We will write the OpenCV math right here next!
+    return rawImageBytes;
+  }
+}
