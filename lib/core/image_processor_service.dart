c85416c02950a4b8a9483ed256c2e2cdc66ecc53
Min Pyae Phyo <subtominthebro@gmail.com>
2026-07-27 16:06:42 +0800

    Finished OCR prototype

--- lib/core/image_processor_service.dart
+++ lib/core/image_processor_service.dart
@@ -1,9 +1,112 @@
 import 'package:opencv_dart/opencv_dart.dart' as cv;
 import 'dart:typed_data';
+import 'dart:io';
+import 'package:google_mlkit_commons/google_mlkit_commons.dart';
+import 'dart:ui'; // uses the Size feature
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart'; //uses the TextRecognizer, RecognizedText features
+//------------------------Version 1 ------------------------------------------------------------------------------
+// class ImageProcessor{
+//   Uint8List processImage(Uint8List rawImageBytes){   //catches raw bytes buffer coming from phone camera
+//     final img = cv.imdecode(rawImageBytes, cv.IMREAD_COLOR);   //imdecode receieves raw byte buffer to decode in memory. Structure raw bytes into Matrix format of 2D pixels (with colors)
+//     final gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY); //takes the img and applies grayscale algo and put it in gray var.
+//     final bgraGray = cv.cvtColor(gray, cv.COLOR_GRAY2BGRA); //recoverting the grayscale image to BGRA (4 channel format) so it will be competible with andriod and IOS. The image is still grayscale
 
-class ImageProcessor{
-  Uint8List processImage(Uint8List rawImageBytes){
-    // We will write the OpenCV math right here next!
-    return rawImageBytes;
+
+//     // var (success, cleanBytes) = cv.imencode('.jpg', gray);   //compressing gray matrix into .jpg format
+//     return bgraGray;
+//   }
+// }
+//=======================================================================================================================
+class ImageProcessor {
+
+  //accepts raw bytes and dimensions from the camera sensors and a flag to identify which os we are reading
+  Uint8List Imageprocess(Uint8List rawBytes, int width, int height, bool isAndroid) {
+    final cv.Mat srcMat;   //initizing var called srcMat
+    final cv.Mat grayMat;
+    if (isAndroid) {      //checking for andriod 
+      srcMat = cv.Mat.fromList((height * 1.5).toInt(), width, cv.MatType.CV_8UC1, rawBytes);   //used 1.5 times height because of andriod 1.5x compressed NV21 format
+      grayMat = cv.cvtColor(srcMat, cv.COLOR_YUV2GRAY_NV21);     //grayscaling the image into grayscale NV21 format
+    } 
+    else {                //checking for IOS
+      srcMat = cv.Mat.fromList(height, width, cv.MatType.CV_8UC4, rawBytes); //used BGRA8888 (4 channel format) for IOS
+      grayMat = cv.cvtColor(srcMat, cv.COLOR_BGRA2GRAY);         //grayscaling the image into grayscale BGRA format
+    }
+    
+    //255 - background to push all the way to pure white
+    //cv.ADAPTIVE_THRESH_GAUSSIAN_C - this calculated a weighted average of the lighting. Used to handle grossy reflections in images.
+    //cv.THRESH_BINARY - forces the image to be white or black. (No gray allowed.)
+    // 21 - size of the local pixel grid (21x21 pixels)
+    final cleanMat = cv.adaptiveThreshold(grayMat, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 21, 10);
+
+    if (isAndroid){
+      Uint8List yBytes = cleanMat.data; // The grayscale image is our Y-plane //Y are one byte per pixel
+      int uvSize = (width * height) ~/ 2; // NV21 UV-plane size because UV colors are one pair for every 2×2 pixels
+    
+      Uint8List nv21Bytes = Uint8List(yBytes.length + uvSize);   //nv21 arrary is a combination of Y scale (brightness) and UV(color)
+
+      nv21Bytes.setRange(0, yBytes.length, yBytes); // 1. fill in the beginning till Yscale (yBytes.length) with grayscale
+      nv21Bytes.fillRange(yBytes.length, nv21Bytes.length, 128); //fill the rest of the NV21 arrary with UV colors which is 128 (color for grayscale images)
+      return nv21Bytes;
+    } else{    
+    //Converting the 1-channel gray binary image back into 4-channel BGRA format to let both andriod and IOS understasnd
+    final finalBgraMat = cv.cvtColor(cleanMat, cv.COLOR_GRAY2BGRA);
+
+    return finalBgraMat.data; // Temporary return so it compiles
+    }
   }
 }
+
+class OCRProcess{
+  //declaring instance for image processor
+  final ImageProcessor _processor = ImageProcessor();
+  final TextRecognizer _textRecognizer = TextRecognizer(script: TextRecognitionScript.latin); //used the TextRecognizer with latin script to read english
+
+  //creating a method for scanLabel operations
+  Future<String> scanLabel(Uint8List rawBytes,int width,int height,bool isAndroid) async {
+    //initlizing a variable with the cleaned image
+    Uint8List cleanBytes = _processor.Imageprocess(rawBytes,width,height,isAndroid);
+
+    //inputing data into ML.
+    //size - size of data
+    //rotation - your rotation of your phone (so your image)
+    //format - BGRA8888 format (because we changed to this format earlier in the ImageProsessor method)
+    //bytesPerRow - BGRA has 4 channel so 4 bytes per pixel
+    final metadata = InputImageMetadata(
+      size: Size(width.toDouble(), height.toDouble()),
+      rotation: InputImageRotation.rotation0deg,
+      format: isAndroid ? InputImageFormat.nv21 : InputImageFormat.bgra8888,
+      bytesPerRow: isAndroid ? width : (width * 4),
+    );
+
+    //Assemble the InputImage using your clean bytes and metadata
+    final inputImage = InputImage.fromBytes(bytes: cleanBytes, metadata: metadata);
+
+    //Feed the 4 channel grayscale image to the ML kit engine
+    final RecognizedText recognizedText = await _textRecognizer.processImage(inputImage);
+
+    //Return the raw extracted string back to the UI
+    return recognizedText.text;
+  
+  }
+}
+//=============ACTUALLY MAIN=================================================
+Future<void> main() async {
+  // 1. Point directly to the test image
+  final inputImage = InputImage.fromFilePath('D:/Internship/flutter_application_1/img/test1.png');
+
+  // 2. Initialize the ML Kit engine to read latin
+  final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin); 
+
+  // 3. Process the image (requires await, which is why main() is now async) used await to pause the code to let the ML process data 
+  print("Scanning image...");
+  final RecognizedText recognizedText = await textRecognizer.processImage(inputImage);
+  // 4. Print the extracted text blocks
+  for (TextBlock block in recognizedText.blocks) {
+    print("Found text: ${block.text}");
+  }
+
+  // 5. Clean up memory
+  textRecognizer.close();
+  print("Process completed.");
+}
+
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,154 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import '../core/image_processor_service.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:io';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+
+  List<CameraDescription> cameras = [];
+  CameraController? cameraController;
+  bool _captureNextFrame = false;
+  final OCRProcess _ocrService = OCRProcess();
+  String _scanResult = "Tap the camera to scan";
+
+  @override
+  void initState(){
+    super.initState();
+    _setupCameraController();
+    
+  }
+  @override
+  void dispose() {
+    cameraController?.dispose();
+    super.dispose();
+  }
+
+  //UI
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: _buildUI(),
+    );
+  }
+  
+  Widget _buildUI(){
+    if (cameraController == null || cameraController?.value.isInitialized == false){
+      return const Center(child: CircularProgressIndicator(),);    
+    }
+    return SafeArea(child: SizedBox.expand(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+            // used expanded and singlechildscroll so the text doesnt overflow
+          Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                  child: Text(
+                    _scanResult,
+                    style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.40,
+            width: MediaQuery.sizeOf(context).width * 0.85,
+            child:ClipRRect(
+              child: FittedBox(
+                fit: BoxFit.cover,
+                alignment: Alignment.center,
+                child: SizedBox(
+                  //because phone are vertically held, sensor height and width are swapped
+                  width: cameraController!.value.previewSize?.height ?? 1 ,
+                  height: cameraController!.value.previewSize?.width ?? 1,
+                  child: CameraPreview(cameraController!),
+                ),
+              ),
+            ) 
+          ),
+        
+          IconButton(
+            onPressed: () {
+              setState(() {
+                _scanResult = "Scanning...";
+                _captureNextFrame = true;
+              });
+            } , 
+            iconSize: 100,            //change button size
+            icon: const Icon(
+              Icons.camera,
+              color: Colors.red,
+            ),
+          )
+        ],
+      ),
+    )
+    ); 
+  }
+
+  Future<void> _setupCameraController() async {
+    List<CameraDescription> _cameras = await availableCameras();    //put list of cameras of device into _cameras
+    if (_cameras.isNotEmpty){               //check if device have cameras
+      setState(() {
+        cameras = _cameras;         //setting cameras list with the list of cameras from _cameras
+        cameraController = CameraController(
+          _cameras.first, // <--- first because backcamera (need to optimized this)
+          ResolutionPreset.high, 
+          enableAudio: false, // turn off microphone 
+          imageFormatGroup: Platform.isAndroid 
+              ? ImageFormatGroup.yuv420     //if andriod hardcode to change format into yuv420
+              : ImageFormatGroup.bgra8888,  //if ios hardcode to change format into BGRA8888
+        );
+      });
+      cameraController?.initialize().then((_) {
+        if (!mounted) return; // Standard safety check
+        setState((){});
+        cameraController?.startImageStream((CameraImage image) async {
+          //deleting the current captured frame to get next frame
+          if (!_captureNextFrame) {
+            return;
+          } 
+          //stop from capturing next frame
+          _captureNextFrame = false;
+          try {
+      
+            // --- THE MEMORY FLATTENER ---
+            final WriteBuffer allBytes = WriteBuffer();
+            for (final Plane plane in image.planes) {
+              allBytes.putUint8List(plane.bytes);
+            }
+            final Uint8List rawBytes = allBytes.done().buffer.asUint8List();
+            
+            final bool isAndroid = Platform.isAndroid;
+
+            // --- FEED THE ENGINE ---
+            final String result = await _ocrService.scanLabel(rawBytes, image.width, image.height, isAndroid);
+            
+            if (mounted) {
+              setState(() {
+                _scanResult = result.isEmpty ? "No text found." : result;
+              });
+            }
+          } catch (e) {
+            if (mounted) {
+              setState(() {
+                _scanResult = "Error: $e";
+              });
+            }
+          }
+        });
+      });
+    }
+  }
+}
